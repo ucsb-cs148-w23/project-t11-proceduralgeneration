9e04add273f3a5add9dbc334652c61313b302b09
Matthew Ho <msho@ucsb.edu>
2023-01-20 02:20:57 -0800

    created basic react app

---
+++ matthew-hello-world/src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App dark fill">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Hello World!
+        </p>
+      </header>
+      <p>
+        Hope this meets the requirements for lab01.
+        <br />
+        - Matthew from team 11
+      </p>
+    </div>
+  );
+}
+
+export default App;

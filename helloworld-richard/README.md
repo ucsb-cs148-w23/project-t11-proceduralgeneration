10bf075b6ed1e4cbec0b28d8992849c8716636ab
boopa5 <rgao.2003@gmail.com>
2023-01-20 01:52:23 -0800

    upload hello world files

---
+++ helloworld-richard/README.md
@@ -0,0 +1,3 @@
+# Hello world webapp - Richard
+
+To use the app, navigate to https://helloworld-66bb0.web.app/
---
+++ helloworld-richard/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world</h1> 
+        <p>
+          Hello world web app on Firebase
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

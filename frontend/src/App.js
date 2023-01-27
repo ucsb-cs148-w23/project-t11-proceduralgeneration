8d2ad211357cb448e99521c93ec79df14db9a17e
Christine Tu <christinetu@umail.ucsb.edu>
2023-01-26 23:30:39 -0800

    create header and model components

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/header.js'
+import Model from './components/model.js'
+import { Canvas } from '@react-three/fiber'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="header" />
+      <Canvas className="model">
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Model position={[-1.2, 0, 0]} />
+        <Model position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header.js
@@ -0,0 +1,15 @@
+
+function header() {
+    return (
+        <div className="header-container">
+            <header className="App-header">
+                <h1>Procedural Generation</h1> 
+                <p>
+                    Three JS Model below
+                </p>
+            </header>
+        </div>
+    );
+}
+  
+export default header;

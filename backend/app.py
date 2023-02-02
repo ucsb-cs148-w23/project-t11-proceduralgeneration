0e9db7e51086c63187513ccb99a8f0f5cfd48049
Matthew Ho <msho@ucsb.edu>
2023-02-02 12:43:48 -0800

    partially working triangless backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 import random
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/generate_map')
 @app.route('/generate_map/<int:scale>')
-def generate_map(scale=8):
+@app.route('/generate_map/<int:scale>/<int:numtriangle>')
+def generate_map(scale=8, numtriangle=1):
     # STUB: return random triangle
     # TODO: convert Andy's proof of concept
-    vertices = [random.randint(-scale, scale) for _ in range(9)]
+    vertices = [random.randint(-scale, scale) for _ in range(9 * numtriangle)]
     return jsonify(vertices=vertices)

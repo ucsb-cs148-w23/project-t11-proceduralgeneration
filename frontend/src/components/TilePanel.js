58dd0eccbff4cb8ca8228df154df76c9ab7aca1f
Matthew Ho <msho@ucsb.edu>
2023-03-02 20:40:35 -0800

    almost all UI done, still need to add logic

--- frontend/src/components/TilePanel.js
+++ frontend/src/components/TilePanel.js
@@ -1,7 +1,9 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, Fragment } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -11,20 +13,33 @@ import Switch from '@mui/material/Switch';
 import { ControlsContext } from '../App.js';
 import TextField from '@mui/material/TextField';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import SaveIcon from '@mui/icons-material/Save';
+import LoopIcon from '@mui/icons-material/Loop';
 import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 import InputSlider from './InputSlider.js'
-
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export default function TilePanel() {
   const { 
-    tilePanel, setTilePanel,
+    setTilePanel,
     tile, setTile,
-    tiles, setTiles,
     neighbor, setNeighbor,
+    tiles, setTiles,
     file2id, setFile2id
   } = useContext(ControlsContext);
 
   const [w, setW] = useState(1);
+  const [addNeighborMode, setAddNeighborMode] = useState(false);
+  const [newNeighborDirection, setNewNeighborDirection] = useState(null);
+  const [newNeighborRotation, setNewNeighborRotation] = useState(0);
+
+  function toggleAddNeighborMode() {
+    setAddNeighborMode(!addNeighborMode);
+  }
 
   function getNeighbors() {
     const tid = file2id[tile];
@@ -55,8 +70,6 @@ export default function TilePanel() {
       <Grid item>
         <ButtonGroup variant="outlined" aria-label="outlined primary button group">
           <Button
-            variant="outlined"
-            color="error"
             startIcon={<ArrowBackIcon />}
             onClick={() => {
               setTilePanel(false);
@@ -67,7 +80,7 @@ export default function TilePanel() {
             Back
           </Button>
           <Button 
-            color="info"
+            startIcon={<AddIcon />}
           >
             New Tile
           </Button>
@@ -76,7 +89,7 @@ export default function TilePanel() {
       <Grid item>
         <Typography 
           variant="h6" 
-          sx={{marginBottom: 1}}
+          sx={{marginBottom: 2}}
         >
           Choose a tile to examine or customize.
         </Typography>
@@ -93,55 +106,139 @@ export default function TilePanel() {
         />
       </Grid>
       {
-        tile && 
-        <Grid item>
-          <FormControlLabel control={<Switch defaultChecked />} label="Include in Generation?" />
-        </Grid>
-      }
-      {
         tile &&
-        <Grid item>
-          <InputSlider 
-            value={w} 
-            setValue={setW} 
-            label="Random Selection Weight" 
-          />
-        </Grid>
-      }
-      {
-        tile &&
-        <Grid item>
-          <Typography 
-            variant="h6" 
-            sx={{marginbottom: 3}}
-          >
-            View or Delete Allowed Neighbors
-          </Typography>
-          <Autocomplete
-            disablePortal
-            options={getNeighbors(tile)}
-            onChange={(event, newValue) => {
-              console.log(newValue);
-              setNeighbor(newValue);
-            }}
-            renderInput={(params) => <TextField {...params} label="Neighbor" />}
-          />
-          <Button startIcon={<AddIcon />}>
-            Add Allowed Neighbor
-          </Button>
+        <Fragment>
+          <Grid item>
+            <FormControlLabel control={<Switch defaultChecked />} label="Include in Generation?" />
+          </Grid>
+          <Grid item>
+            <InputSlider 
+              value={w} 
+              setValue={setW} 
+              label="Random Selection Weight" 
+            />
+          </Grid>
+          <Divider />
+          <Grid item>
+            <Typography 
+              variant="h6" 
+            >
+              Add or Remove Allowed Neighbors
+            </Typography>
+            <Box sx={{marginBottom: 2}}></Box>
+            {
+              !addNeighborMode ?
+              <Fragment>
+                <Autocomplete
+                  disablePortal
+                  options={getNeighbors(tile)}
+                  onChange={(event, newValue) => {
+                    console.log(newValue);
+                    setNeighbor(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} label="Neighbor" />}
+                />
+                <Button 
+                  startIcon={<AddIcon />} 
+                  onClick={() => {
+                    setNeighbor(null);
+                    setAddNeighborMode(true);
+                  }} >
+                  Add Allowed Neighbor
+                </Button>
+              </Fragment>
+              : 
+              <Fragment>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Direction</InputLabel>
+                  <Select
+                    label="Direction"
+                    id="demo-simple-select"
+                    labelId="demo-simple-select-label"
+                    value={newNeighborDirection}
+                    onChange={(event) => {
+                      console.log(event.target.value);
+                      setNewNeighborDirection(event.target.value);
+                      if (neighbor) {
+                        const updated = {...neighbor};
+                        updated["direction"] = event.target.value;
+                        setNeighbor(updated);
+                      }
+                    }}
+                  >
+                    <MenuItem value={"px"}>Front (px)</MenuItem>
+                    <MenuItem value={"nx"}>Back (nx)</MenuItem>
+                    <MenuItem value={"py"}>Right (py)</MenuItem>
+                    <MenuItem value={"ny"}>Left (ny)</MenuItem>
+                    <MenuItem value={"pz"}>Above (pz)</MenuItem>
+                    <MenuItem value={"nz"}>Below (nz)</MenuItem>
+                  </Select>
+                </FormControl>
+                <Box sx={{marginBottom: 2}}></Box>
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  options={Object.values(tiles)}
+                  onChange={(event, newValue) => {
+                    // console.log(newValue["mesh"]);
+                    const id = file2id[newValue["mesh"]];
+                    const updated = {
+                      "label": newValue["mesh"],
+                      "id": id,
+                      "direction": newNeighborDirection,
+                      "rotation": newNeighborRotation
+                    };
+                    setNeighbor(updated);
+                  }}
+                  renderInput={(params) => <TextField {...params} label="New Neighbor" />}
+                />
+              </Fragment>
+            }
 
-        </Grid>
+          </Grid>
+        </Fragment>
       }
       {
         neighbor &&
         <Grid item>
           <ButtonGroup variant="outlined" aria-label="outlined primary button group">
-            <Button color="error">Delete Neighbor</Button>
             <Button
-              onClick={() => {setNeighbor(null)}}
+              onClick={() => {
+                setNeighbor(null);
+                setAddNeighborMode(false);
+              }}
+              startIcon={<ArrowBackIcon />}
             >
               Cancel
             </Button>
+            {
+              addNeighborMode ?
+              <Fragment>
+                <Button
+                  startIcon={<LoopIcon />}
+                  onClick={() => {
+                    if (neighbor) {
+                      const updated = {...neighbor};
+                      updated["rotation"] = (updated["rotation"] + 1) % 4;
+                      setNeighbor(updated);
+                      setNewNeighborRotation(updated["rotation"]);
+                    }
+                  }}
+                >
+                  Rotate
+                </Button>
+                <Button
+                  startIcon={<SaveIcon />}
+                >
+                  Save
+                </Button>
+              </Fragment>
+              : <Button
+                startIcon={<DeleteIcon />}
+              >
+                Delete Neighbor
+              </Button>
+            }
           </ButtonGroup>
         </Grid>
       }
